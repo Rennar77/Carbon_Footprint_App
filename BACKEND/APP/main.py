0e984d1dce9bda8b6c3a455b48b5e8114a848f53
Rennar77 <renardowiti@gmail.com>
2025-11-14 21:23:11 +0300

    update main.py

--- BACKEND/APP/main.py
+++ BACKEND/APP/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from services import vehicle_service, log_service, auth_service, summary_service
-from routes.auth_routes import router as auth_router
-from routes.recommendation_routes import router as recommendation_router  
+from app.services import vehicle_service, log_service, auth_service, summary_service
+from app.routes.auth_routes import router as auth_router
+from app.routes.recommendation_routes import router as recommendation_router
 
 app = FastAPI(title="Carbon Footprint API")
 
@@ -21,8 +21,7 @@ app.include_router(auth_router, prefix="/auth", tags=["Authentication"])
 app.include_router(log_service.router, prefix="/log", tags=["Logging"])
 app.include_router(summary_service.router, prefix="/dashboard", tags=["Summary"])
 app.include_router(vehicle_service.router, tags=["Vehicles"])
-app.include_router(recommendation_router, prefix="/api", tags=["Recommendations"])  
-app.include_router(auth_router, prefix="/api")
+app.include_router(recommendation_router, prefix="/api", tags=["Recommendations"])
 
 @app.get("/")
 def home():
